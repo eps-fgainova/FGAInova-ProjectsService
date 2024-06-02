941eae0f7f9454fa76c4d20fd4166b2cec710338
Pedro Lima <phlimas@outlook.com>
2024-06-01 23:04:50 -0300

    feat: Ajuste para deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4444;
 
 try {
   mongoose
@@ -33,3 +33,5 @@ app.use(express.json());
 
 // Rotas
 app.use('/', router);
+
+module.exports = app;
